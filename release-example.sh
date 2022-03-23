69ad08bf9a0ecd95d29ec053294da267d09badf4
omri lugasi <omriDroid@gmail.com>
2022-03-23 09:47:02 +0200

    F/25 (#21)
    * docs:  add nvmrc file
    
    * fix:  add nvmrc file
    
    * style:  add nvmrc file
    
    * style:  add test file
    
    Co-authored-by: omri <omlrc1991@gmail.com>

--- release-example.sh
+++ release-example.sh
@@ -10,28 +10,28 @@ node -v > echo '.nvmrc$1'
 
 git add .
 
-git commit -m "docs(nvmrc):  add nvmrc file"
+git commit -m "docs:  add nvmrc file"
 
 
 date > echo
 
 git add .
 
-git commit -m "fix(nvmrc):  add nvmrc file"
+git commit -m "fix:  add nvmrc file"
 
 
 node -v > echo
 
 git add .
 
-git commit -m "style(nvmrc):  add nvmrc file"
+git commit -m "style:  add nvmrc file"
 
 
 date > echo
 
 git add .
 
-git commit -m "style(test):  add test file"
+git commit -m "style:  add test file"
 
 git push --set-upstream origin "f/$1"
 
