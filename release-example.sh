9704b59c83ba5890806b2291963feae574ddcf87
omri <omlrc1991@gmail.com>
2022-03-23 09:41:51 +0200

    docs(nvmrc):  add nvmrc file

--- release-example.sh
+++ release-example.sh
@@ -9,21 +9,21 @@ git add .
 git commit -m "docs(nvmrc):  add nvmrc file"
 
 
-node -v > echo '.nvmrc_fix$1'
+node -v > echo
 
 git add .
 
 git commit -m "fix(nvmrc):  add nvmrc file"
 
 
-node -v > echo '.nvmrc_style$1'
+node -v > echo
 
 git add .
 
 git commit -m "style(nvmrc):  add nvmrc file"
 
 
-node -v > echo '.nvmrc_test$1'
+node -v > echo
 
 git add .
 
