71eb59180f07db3454054f24ef8286205fd13f39
omri <omlrc1991@gmail.com>
2022-04-01 16:20:44 +0300

    fix: change the way we work with commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      "feat",
+      "fix",
+      "docs",
+      "refactor",
+      "style",
+      "perf",
+      "test",
+      "chore"
+    ]]
+  },
+}
